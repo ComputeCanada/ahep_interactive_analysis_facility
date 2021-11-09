a67e83f84420fa8b4724781365f65ec7a2521c1d
tejinc <tejinc.a1@gmail.com>
2021-11-09 11:23:55 -0500

    update Docker

--- image/kubernetes-image/base-notebook/start.sh
+++ image/kubernetes-image/base-notebook/start.sh
@@ -42,23 +42,23 @@ run-hooks /usr/local/bin/start-notebook.d
 # Handle special flags if we're root
 if [ "$(id -u)" == 0 ] ; then
 
-    # Only attempt to change the jovyan username if it exists
-    if id jovyan &> /dev/null ; then
+    # Only attempt to change the jupyter username if it exists
+    if id jupyter &> /dev/null ; then
         echo "Set username to: ${NB_USER}"
-        usermod -d "/home/${NB_USER}" -l "${NB_USER}" jovyan
+        usermod -d "/home/${NB_USER}" -l "${NB_USER}" jupyter
     fi
 
     # handle home and working directory if the username changed
-    if [[ "${NB_USER}" != "jovyan" ]]; then
+    if [[ "${NB_USER}" != "jupyter" ]]; then
         # changing username, make sure homedir exists
         # (it could be mounted, and we shouldn't create it if it already exists)
         if [[ ! -e "/home/${NB_USER}" ]]; then
             echo "Copying home dir to /home/${NB_USER}"
             mkdir "/home/${NB_USER}"
-            cp -a /home/jovyan/. "/home/${NB_USER}/" || ln -s /home/jovyan "/home/${NB_USER}"
+            cp -a /home/jupyter/. "/home/${NB_USER}/" || ln -s /home/jupyter "/home/${NB_USER}"
         fi
-        # if workdir is in /home/jovyan, cd to /home/${NB_USER}
-        if [[ "${PWD}/" == "/home/jovyan/"* ]]; then
+        # if workdir is in /home/jupyter, cd to /home/${NB_USER}
+        if [[ "${PWD}/" == "/home/jupyter/"* ]]; then
             newcwd="/home/${NB_USER}/${PWD:13}"
             echo "Setting CWD to ${newcwd}"
             cd "${newcwd}"
@@ -105,7 +105,7 @@ if [ "$(id -u)" == 0 ] ; then
     echo "Executing the command:" "${cmd[@]}"
     exec sudo -E -H -u "${NB_USER}" PATH="${PATH}" XDG_CACHE_HOME="/home/${NB_USER}/.cache" PYTHONPATH="${PYTHONPATH:-}" "${cmd[@]}"
 else
-    if [[ "${NB_UID}" == "$(id -u jovyan 2>/dev/null)" && "${NB_GID}" == "$(id -g jovyan 2>/dev/null)" ]]; then
+    if [[ "${NB_UID}" == "$(id -u jupyter 2>/dev/null)" && "${NB_GID}" == "$(id -g jupyter 2>/dev/null)" ]]; then
         # User is not attempting to override user/group via environment
         # variables, but they could still have overridden the uid/gid that
         # container runs as. Check that the user has an entry in the passwd
@@ -114,8 +114,8 @@ else
         if [[ "${STATUS}" != "0" ]]; then
             if [[ -w /etc/passwd ]]; then
                 echo "Adding passwd file entry for $(id -u)"
-                sed -e "s/^jovyan:/nayvoj:/" /etc/passwd > /tmp/passwd
-                echo "jovyan:x:$(id -u):$(id -g):,,,:/home/jovyan:/bin/bash" >> /tmp/passwd
+                sed -e "s/^jupyter:/nayvoj:/" /etc/passwd > /tmp/passwd
+                echo "jupyter:x:$(id -u):$(id -g):,,,:/home/jupyter:/bin/bash" >> /tmp/passwd
                 cat /tmp/passwd > /etc/passwd
                 rm /tmp/passwd
             else
@@ -124,7 +124,7 @@ else
         fi
 
         # Warn if the user isn't going to be able to write files to ${HOME}.
-        if [[ ! -w /home/jovyan ]]; then
+        if [[ ! -w /home/jupyter ]]; then
             echo 'Container must be run with group "users" to update files'
         fi
     else
